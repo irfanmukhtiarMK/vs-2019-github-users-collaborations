f1626559a6b668fdf394f108ba79575a9a5812b8
irfanmukhtiarMK <testirfanmukhtiar@gmail.com>
2021-10-11 16:53:52 +0500

    Lets commint and push stage changes in 2 files

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace vs_2019_github_users_collaborations
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, startup staging , see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
